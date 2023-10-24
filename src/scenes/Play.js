524deec1d934af7579b12704521056d1a412eaef
sarowley <sarowley@ucsc.edu>
2023-10-24 12:45:20 -0700

    added camera movement

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,19 +6,29 @@ class Play extends Phaser.Scene {
     preload() {}
     create() {
 
-        dude = this.physics.add.sprite(100, 450, 'image');
+        dude = this.physics.add.sprite(450, 550, 'image');
 
         platforms = this.physics.add.staticGroup();
 
-        platforms.create(400, 568, 'image').setScale(2).refreshBody();
+        this.cameras.main.setBounds(-500, -500 , 1600, 1200)
+        this.cameras.main.startFollow(dude);
 
-        platforms.create(100, 600, 'image');
-        platforms.create(200, 600, 'image');
+        platforms.create(400, 980, 'image').setScale(25).refreshBody();
+
+        // function getRandom(min, max) {
+        //     return Math.random() * (max - min) + min;
+        // }
+
+        // for (let x = 0; x < 30; x++){
+        //     platforms.create(getRandom(0, 800), getRandom(0, 600), 'image');
+        // }
+
+        platforms.create(700, 500, 'image');
+        platforms.create(200, 450, 'image');
         platforms.create(300, 550, 'image');
         platforms.create(600, 450, 'image');
 
-        //dude.setBounce(0.2);
-        dude.setCollideWorldBounds(true);
+
 
         this.physics.add.collider(dude, platforms);
 
